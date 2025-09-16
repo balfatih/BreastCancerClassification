75a324c85a4ad8cb6770f5762e129010cfa2cbbe
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-09-16 11:30:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,22 +17,18 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 
 if uploaded_file is not None:
-    # Görüntüyü aç
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # Ön işleme: eğitimde yaptığınız gibi normalize edin
     image = image.resize((128, 128))
     img_array = np.array(image).astype("float32") / 255.0
-    img_array = np.expand_dims(img_array, axis=0)  # (1,128,128,3)
+    img_array = np.expand_dims(img_array, axis=0)
 
-    # Tahmin
     prediction = model.predict(img_array)
 
     st.subheader("Prediction Result")
     st.write(f"Predicted Class: **{prediction[0]}**")
 
-    # Eğer olasılık da görmek isterseniz
     if hasattr(model, "predict_proba"):
         proba = model.predict_proba(img_array)
         st.write("Class Probabilities:")
