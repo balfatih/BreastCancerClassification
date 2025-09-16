fea2c6ffbb934b193d4888f537dd9b71414d28ac
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-09-16 11:32:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ st.title('Breast Cancer Classification')
 st.write("Upload a breast image and the model will classify it.")
 
 model = joblib.load("CNN_GBM_model.joblib")
+st.write("Input shape to model:", img_array.shape)
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
