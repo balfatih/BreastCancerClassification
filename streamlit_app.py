4676f1c1d8257f546d117701b38ecf7087342511
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-09-16 11:08:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import streamlit as st 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+import numpy as np
+import joblib
+from sklearn.preprocessing import StandardScaler
 
+st.title('Breast Cancer Classification')
