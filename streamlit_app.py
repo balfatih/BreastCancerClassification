e996c078d2d6520dad056011f5a29012339ab4fb
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-09-16 11:51:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,18 +10,18 @@ from PIL import Image
 
 st.title('Breast Cancer Classification')
 st.write("Upload a breast image and the model will classify it.")
-
+ 
 
 # ----------------------
 # CNN Model Yükleme
 # ----------------------
-@st.cache_resource
 def load_cnn_model():
     model_path = "cnn_model.h5"
     if not os.path.exists(model_path):
-        url = "https://drive.google.com/uc?id=18TuFKSnLCySlrdiZ8M-ySIRqI5adu3oW"
+        url = "https://drive.google.com/file/d/18TuFKSnLCySlrdiZ8M-ySIRqI5adu3oW/"
+        import gdown
         gdown.download(url, model_path, quiet=False)
+    import tensorflow as tf
     return tf.keras.models.load_model(model_path)
 
 cnn_model = load_cnn_model()
-
