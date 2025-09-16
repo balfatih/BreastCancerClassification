2111fe466c3c12f877fccc408c1ccbe0b5d73e21
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-09-16 13:11:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,14 @@ st.write("Upload a breast image and the model will classify it.")
 # ----------------------
 # CNN Model Yükleme
 # ----------------------
-
 model_path = tf.keras.models.load_model("CNN_Model.h5")
+
+# ----------------------
+# GBM Model Yükleme
+# ----------------------
+@st.cache_resource
+def load_gbm_model():
+    return joblib.load("CNN_GBM_model.joblib")
+
+gbm_model = load_gbm_model()
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
